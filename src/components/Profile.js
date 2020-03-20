bece6980275576949c821ca7ba444d21e7d7a4bb
Avi <aviagarwal100@gmail.com>
2020-03-19 17:01:49 -0700

    commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -22,7 +22,7 @@ class Profile extends Component {
     formData.append("user", user);
     formData.append("title", this.state.title);
     const config = {
-      mode: "no-cors",
+      crossDomain: true,
       headers: {
         "content-type": "multipart/form-data"
         
