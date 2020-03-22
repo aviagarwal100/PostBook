ee64fc44134793cc3fa9f3d634ee0e993dd2dd12
Avi <aviagarwal100@gmail.com>
2020-03-21 20:58:50 -0700

    commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port =process.env.PORT||3000;
 const  publicPath=path.join(__dirname,"/build");
 
 
-app.use('/counselling/triage/',express.static(publicPath));
+app.use('/',express.static(publicPath));
 app.get("*",(req,res)=>{
     res.sendFile(path.join(publicPath,"/index.html"));
 })
